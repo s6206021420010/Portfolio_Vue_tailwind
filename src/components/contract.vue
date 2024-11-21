acdaa055714656060a32a83181c1e59f5235bdfd
taratep <83873963+s6206021420010@users.noreply.github.com>
2024-11-22 00:34:46 +0700

    maincode

---
+++ src/components/contract.vue
@@ -0,0 +1,271 @@
+<template>
+  <div class="flex gap-2">
+    <button class="button" @click="GithubOutlined()">
+        <span class="button_lg">
+            <span class="button_sl"></span>
+            <span class="button_text ">
+                <div class="d-flex justify-content-center align-items-center">
+                    <GithubOutlined class="fs-1 me-2" />
+                    <span>Git hub</span>
+                </div>
+            </span>
+        </span>
+    </button>
+    <button class="button ms-2" @click="LinkedinOutlined()">
+        <span class="button_lg">
+            <span class="button_sl"></span>
+            <span class="button_text ">
+                <div class="d-flex justify-content-center align-items-center">
+                    <LinkedinOutlined class="fs-1 me-2" />
+                    <span>Linkin</span>
+                </div>
+            </span>
+        </span>
+    </button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+      GithubOutlined(){
+        location.href = "https://github.com/s6206021420010"
+      },
+      LinkedinOutlined(){
+        location.href = "https://www.linkedin.com/in/taratep-kerdboon-a0b46826b/"
+      }
+    }
+}
+</script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Figtree&display=swap");
+
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+
+.button {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    border: none;
+    background: none;
+    color: #0f1923;
+    cursor: pointer;
+    position: relative;
+    padding: 8px;
+    margin-bottom: 20px;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 14px;
+    transition: all .15s ease;
+    width: 50%;
+}
+
+.button::before,
+.button::after {
+    content: '';
+    display: block;
+    position: absolute;
+    right: 0;
+    left: 0;
+    height: calc(50% - 5px);
+    border: 1px solid #7D8082;
+    transition: all .15s ease;
+}
+
+.button::before {
+    top: 0;
+    border-bottom-width: 0;
+}
+
+.button::after {
+    bottom: 0;
+    border-top-width: 0;
+}
+
+.button:active,
+.button:focus {
+    outline: none;
+}
+
+.button:active::before,
+.button:active::after {
+    right: 3px;
+    left: 3px;
+}
+
+.button:active::before {
+    top: 3px;
+}
+
+.button:active::after {
+    bottom: 3px;
+}
+
+.button_lg {
+    position: relative;
+    display: block;
+    padding: 10px 20px;
+    color: #fff;
+    background-color: #0f1923;
+    overflow: hidden;
+    box-shadow: inset 0px 0px 0px 1px transparent;
+}
+
+.button_lg::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 2px;
+    height: 2px;
+    background-color: #0f1923;
+}
+
+.button_lg::after {
+    content: '';
+    display: block;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 4px;
+    height: 4px;
+    background-color: #0f1923;
+    transition: all .2s ease;
+}
+
+.button_sl {
+    display: block;
+    position: absolute;
+    top: 0;
+    bottom: -1px;
+    left: -8px;
+    width: 0;
+    background-color: #ff4655;
+    transform: skew(-15deg);
+    transition: all .2s ease;
+}
+
+.button_text {
+    position: relative;
+}
+
+.button:hover {
+    color: #0f1923;
+}
+
+.button:hover .button_sl {
+    width: calc(100% + 15px);
+}
+
+.button:hover .button_lg::after {
+    background-color: #fff;
+}
+
+.nm_text {
+    color: #a5dbff !important;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Figtree", sans-serif;
+}
+
+body {
+    display: grid;
+    place-content: center;
+    min-height: 100vh;
+    background: #000 !important;
+}
+
+.container {
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    gap: 1em;
+    width: 800px;
+    height: 500px;
+    transition: all 400ms;
+}
+
+.container:hover .box {
+    filter: grayscale(100%) opacity(24%);
+}
+
+.box {
+    position: relative;
+    background: var(--img) center center;
+    background-size: cover;
+    transition: all 400ms;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.container .box:hover {
+    filter: grayscale(0%) opacity(100%);
+}
+
+.container:has(.box-1:hover) {
+    grid-template-columns: 3fr 1fr 1fr 1fr 1fr;
+}
+
+.container:has(.box-2:hover) {
+    grid-template-columns: 1fr 3fr 1fr 1fr 1fr;
+}
+
+.container:has(.box-3:hover) {
+    grid-template-columns: 1fr 1fr 3fr 1fr 1fr;
+}
+
+.container:has(.box-4:hover) {
+    grid-template-columns: 1fr 1fr 1fr 3fr 1fr;
+}
+
+.container:has(.box-5:hover) {
+    grid-template-columns: 1fr 1fr 1fr 1fr 3fr;
+}
+
+.box:nth-child(odd) {
+    transform: translateY(-16px);
+}
+
+.box:nth-child(even) {
+    transform: translateY(16px);
+}
+
+.box::after {
+    content: attr(data-text);
+    position: absolute;
+    bottom: 20px;
+    background: #000;
+    color: #fff;
+    padding: 10px 10px 10px 14px;
+    letter-spacing: 4px;
+    text-transform: uppercase;
+    transform: translateY(60px);
+    opacity: 0;
+    transition: all 400ms;
+}
+
+.box:hover::after {
+    transform: translateY(0);
+    opacity: 1;
+    transition-delay: 400ms;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="bg-gray-100 min-h-screen flex justify-center py-0 px-4 relative">
+      <div class="bg-white shadow-md rounded-lg w-full max-w-4xl p-6">
+          <!-- Header -->
+          <header class="flex flex-col md:flex-row items-center border-b pb-4 mb-6 relative">
+              <img src="../assets/my_img.jpg" alt="Profile Picture" class="rounded-full w-24 h-25 object-cover mr-0 md:mr-6 mb-4 md:mb-0" />
+              <div class="text-center md:text-left">
+                  <h1 class="text-2xl font-bold text-gray-800">TARATEP KERDBOON</h1>
+                  <p class="text-gray-500">Software Developer</p>
+                  <p class="text-gray-500">taratep.dev1997@gmail.com | 092-7604466</p>
+              </div>
+              <Contract class="w-full  " />
+          </header>
+  
+          <!-- Content -->
+          <div>
+              <!-- Section: Summary -->
+              <section class="mb-6">
+                  <h2 class="text-xl font-semibold text-gray-800 border-b pb-2">Profile</h2>
+                  <p class="text-gray-600 mt-2">
+                      I’m a Software Developer with strong problem-solving skills, and I’m passionate about creating beautiful and user-friendly websites. I excel at developing efficient solutions that meet the requirements effectively.
+                  </p>
+              </section>
+  
+              <!-- Section: Skills -->
+              <section class="mb-6">
+                  <h2 class="text-xl font-semibold text-gray-800 border-b pb-2">Skills</h2>
+                  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-2">
+                      <div>
+                          <span class="text-gray-900 font-semibold">Web Technologies</span>
+                          <ul>
+                              <li class="text-gray-600 pl-2">PHP</li>
+                              <li class="text-gray-600 pl-2">JavaScript ES6</li>
+                              <li class="text-gray-600 pl-2">HTML & CSS</li>
+                          </ul>
+                      </div>
+                      <div>
+                          <span class="text-gray-900 font-semibold">JavaScript Library</span>
+                          <ul>
+                              <li class="text-gray-600 pl-2">jQuery</li>
+                              <li class="text-gray-600 pl-2">Ant Design</li>
+                              <li class="text-gray-600 pl-2">VueJS</li>
+                              <li class="text-gray-600 pl-2">ReactJS</li>
+                              <li class="text-gray-600 pl-2">React Native</li>
+                          </ul>
+                      </div>
+                      <div>
+                          <span class="text-gray-900 font-semibold">Frameworks</span>
+                          <ul>
+                              <li class="text-gray-600 pl-2">BootStrap</li>
+                              <li class="text-gray-600 pl-2">Tailwind</li>
+                          </ul>
+                      </div>
+                      <div>
+                          <span class="text-gray-900 font-semibold">Other Tools</span>
+                          <ul>
+                              <li class="text-gray-600 pl-2">Version Control (Git), Remote Desktop</li>
+                              <li class="text-gray-600 pl-2">Terminus</li>
+                              <li class="text-gray-600 pl-2">MySQL</li>
+                          </ul>
+                      </div>
+                  </div>
+              </section>
+  
+              <!-- Section: Experience -->
+              <section class="mb-6">
+                  <h2 class="text-xl font-semibold text-gray-800 border-b pb-2">Education</h2>
+                  <div class="mt-2">
+                      <div class="mb-4">
+                          <h3 class="font-semibold text-gray-700">KING MONGKUT'S UNIVERSITY OF TECHNOLOGY NORTH BANGKOK</h3>
+                          <p class="text-gray-500 text-sm">Jun 2019 - Jun 2022</p>
+                          <ul>
+                              <li class="text-gray-600 pl-2">Information Technology</li>
+                              <li class="text-gray-600 pl-2">GPA: 2.56</li>
+                          </ul>
+                      </div>
+                  </div>
+              </section>
+  
+              <!-- Section: Career -->
+              <section>
+                  <h2 class="text-xl font-semibold text-gray-800 border-b pb-2">CAREER</h2>
+                  <div class="space-y-6">
+                      <div>
+                          <h3 class="font-semibold text-gray-700">Web Developer | L&E Manufacturing CO.,LTD</h3>
+                          <p class="text-gray-500 text-sm">Sep 2022 - Jun 2023</p>
+                          <ul>
+                              <li class="text-gray-600 pl-2">Collaborate in the development of Enterprise Resource Planning (ERP) systems.</li>
+                              <li class="text-gray-600 pl-2">Participate in the development of payroll calculation systems.</li>
+                              <li class="text-gray-600 pl-2">Take responsibility for developing employee fingerprint scanning systems.</li>
+                              <li class="text-gray-600 pl-2">Provide support and troubleshoot various issues for employees and users within the company.</li>
+                          </ul>
+                      </div>
+                      <div>
+                          <h3 class="font-semibold text-gray-700">Front-end Developer | Censoft Enterprise Software Co.,Ltd.</h3>
+                          <p class="text-gray-500 text-sm">Jun 2023 - Present</p>
+                          <ul>
+                              <li class="text-gray-600 pl-2">Rajavej Hospital Bangkok: Developed front-end systems for Rajavej Hospital using HTML, Bootstrap, and VueJS.</li>
+                              <li class="text-gray-600 pl-2">National Cancer Institute: Designed front-end systems for the National Cancer Institute using HTML, Bootstrap, and VueJS.</li>
+                              <li class="text-gray-600 pl-2">Political Party: Developed front-end systems and contributed to back-end development using Python and PHP.</li>
+                          </ul>
+                      </div>
+                  </div>
+              </section>
+          </div>
+      </div>
+  </div>
+  </template>
+  
+
+<script>
+import Contract from "../components/contract.vue";
+export default {
+    components: {
+        Contract,
+    },
+};
+</script>
+
+<style scoped>
+</style>
